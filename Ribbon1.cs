3e0ead39236f19f3fc6c571358eeaf81122f8221
Ioan Moldovan <mldvnioan@mldvnioan.onmicrosoft.com>
2020-01-26 22:18:07 -0800

    First Commit for outlock add in

---
+++ Ribbon1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace vsto1
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+
+            System.Windows.Forms.MessageBox.Show("it works ");
+            string[] lines = { "Carmen i love you ", "Elisa i Love you", "Evelyn i Love you " };
+            System.IO.File.WriteAllLines(@"C:\Users\mldvn\test\WriteLines.txt", lines);
+        }
+    }
+}
